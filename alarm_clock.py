f1be2346fbd96602199d980c133af13522d486d9
Damir Abdulin <damirabdulin2@gmail.com>
2019-08-06 15:09:09 +0300

    Add take_sleep_time.

--- alarm_clock.py
+++ alarm_clock.py
@@ -1,4 +1,5 @@
 import pyglet
+import time
 
 def play_song(signal_name):
     ''' Plays signal. '''
@@ -7,3 +8,16 @@ def play_song(signal_name):
     song.play()
 
     pyglet.app.run()
+
+def take_sleep_time():
+    ''' Ask, how many time you want sleep. '''
+    
+    print("How many time you want sleep?")
+    
+    hours = input("How many hours you want sleep?")
+    minutes = input("How many minutes you want sleep?")
+    
+    sleep_time = (hours * 60 + minutes) * 60
+    
+    return sleep_time 
+    
